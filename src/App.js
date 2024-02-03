46903fd8d4bcc9de8b99c01d423c373cfa9440dd
Arun Kumar R <aruntutter19@gmail.com>
2024-02-03 13:08:09 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>React Cart</h1>
+      {/* Navbar */}
+      <nav>
+        <h1>React-CART</h1>
+        <div className="cart-icon">
+          <img
+            src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_cart-eed150.svg"
+            alt="cart-icon"
+          />
+          <span className="cart-total-count">0</span>
+        </div>
+      </nav>
+
+      {/* Products */}
     </div>
   );
 }
